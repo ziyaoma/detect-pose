1a928b9275475711fd016f6d44d9242ebe221e35
ziyaoma <39823610+ziyaoma@users.noreply.github.com>
2024-01-19 15:08:24 +0800

    Update val.py

--- yolov8-pose/val.py
+++ yolov8-pose/val.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import os
 
 model = YOLO('weights/best.pt')
-metrics = model.val(data='lsp-pose10.yaml',imgsz=224)  # no arguments needed, dataset and settings remembered
+metrics = model.val(data='lsp-pose.yaml',imgsz=224)  # no arguments needed, dataset and settings remembered
 # metrics.box.map    # map50-95
 # metrics.box.map50  # map50
 # metrics.box.map75  # map75
