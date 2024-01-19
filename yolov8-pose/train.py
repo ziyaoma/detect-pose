b652783af1e14784d9f681fb624eee8692115777
ziyaoma <39823610+ziyaoma@users.noreply.github.com>
2024-01-19 15:09:51 +0800

    Update train.py

--- yolov8-pose/train.py
+++ yolov8-pose/train.py
@@ -2,8 +2,7 @@ from ultralytics import YOLO
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-#model = YOLO(current_dir+'/coco-pose.yaml').load(current_dir+'/yolov8s-pose.pt')  # build from YAML and transfer weights
-
+model = YOLO('yolov8s-pose.yaml').load('yolov8s-pose.pt')
 # Train the model
-results = model.train(data=current_dir+'/coco-pose.yaml',
-                      epochs=100, imgsz=224,workers=1,batch=1)
+results = model.train(data=current_dir+'/lsp-pose.yaml',
+                      epochs=300, imgsz=224,workers=1,batch=16)
